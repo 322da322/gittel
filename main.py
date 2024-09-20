f3efccb7c48bcf4244ba5627fdbf7385f5492778
Alexhardim <alexhardim20@gmail.com>
2024-09-20 22:46:13 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("123123")
+import logging
+import datetime
+from aiogram import Bot, Dispatcher, executor, types
+
+
+# курс доллара
+
+# Настойка бота
+# ----------------------------
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token="6105269350:AAFdfI9vMQ77TUuv2sJXewfDg82SbPml5po")
+
+dp = Dispatcher(bot)
+
+# ---------------------------------------------
+
+
+
+# стартовая функция
+@dp.message_handler(commands=['start'], commands_prefix='/')
+async def process_start_command(message: types.Message):
+    await message.answer("qweqwewq")
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
