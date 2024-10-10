8a4d6d837e29900a51fb3894653cb9155062bdad
Alexhardim <alexhardim20@gmail.com>
2024-10-10 21:28:05 +0300

    com

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ async def yes(callback: types.CallbackQuery):
 @dp.callback_query_handler(text='1')
 async def yes(callback: types.CallbackQuery):
     await callback.message.answer(parsing.qeustion[random.randint(1,50)])
-    flagQue = True
+
 
 async def greet(message: types.Message):
     
